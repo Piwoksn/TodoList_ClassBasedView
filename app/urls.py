65ef93b69c5019e77462a793d9728f29e4f85719
Piwoksn <piwoksn@gmail.com>
2025-04-11 16:45:11 +0100

    file updated

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
     path("<int:pk>/", views.TaskDetail.as_view(), name="detail"),
